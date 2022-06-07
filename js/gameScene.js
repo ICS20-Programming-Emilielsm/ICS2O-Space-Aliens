f938d7b745538583aaf236a9063d94f0ea936dfd
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-06-07 14:31:40 +0000

    edit

--- js/gameScene.js
+++ js/gameScene.js
@@ -9,6 +9,10 @@
 class GameScene extends Phaser.Scene {
   constructor () {
     super({key: 'gameScene'})
+
+    this.titleSceneBackgroundImage = null
+    this.ship = null
+
   }
 
   init (data) {
@@ -19,12 +23,40 @@ class GameScene extends Phaser.Scene {
   // console message
   preload () {
     console.log("Emilie's Game Scene")
-  }
+    //images
+    this.load.image('starBackground', 'assets/starBackground.png')
+    this.load.image('ship', 'assets/spaceShip.png')
+    this.load.image('missile', 'assets/missile.png')
 
+  }
+  // Background image
   create (data) {
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
+    // SPACE SHIP STARTING LOCATION 
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
   }
 
   update (time, delta) {
+    // called 60 times a second
+    //movement commands
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    const keySpaceObj = this.input.keyboard.addKey('SPACE')
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
   }
 }
 
